44d3e402bf2f0a9b46846d13783e7aa31bc9476f
Arturo Salmeron <asalmeron3@gatech.edu>
2017-09-06 19:26:27 -0400

    TriviaGameBasic

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # TriviaGame
+For the Javascript/Jquery needed for this game:
+
+1. Make a set of 10 questions that the user can answer; I will use an ordered list to display each question.
+
+2. Each question should have four (4) potential answers; the user can only select 1 of these :use unordered list for the answer choices as well as radio inputs.
+
+3. To store my questions and answer choices, I will create an object that alternates between the question and the potential answers. 
+
+4. For iterative purposes: The name for the element that holds the questions will be 'Q1' , 'Q2', 'Q3', etc. The answer choices will follow a similar pattern: ans1, ans2, ans3...
+
+5. Making a global variable that will store the solutions to the questions in the form of the string  a, b, c, or d. I am chosing these four (4) string characters because I will also be using them later to give each radio input these same values
+
+6.Making a function that will iteratively go through my object and compose the html needed to diplay to the DOM.
+	a. Loop through half the length of the object (half because half of the object is questions, the other half is made of arrays of choices)
+	b. Store the html in a string; index the question in the object and add as part of html
+	c. loop through the array of choices in the object. 
+	d. Add the html with each answer choice to the string of HTML.
+	e. Add the string of closing tags for your html.
+	f. Append the HTML to div that will have the question/answers
+	g. repeat (well the loop will repeat for you)
+
+7. Make global variables for the # of correct answers, wrong answers, and unanswered. Also need one for the amount of seconds the user will be given in the game, and once that will store the setInterval 
+
+8. On clicking the button (".start"), run a function that:
+	a. hides the start buttion
+	b. Will display a countdown to the DOM based on the current time/count. This can be done by adding html to the div ".time". The html should call the current count
+	c. run the function that interatively writed your questions/answer-choices to the DOM
+	d. Runs a function the keepGoing (shows all questinos and timer)
+
+9. In the keepGoing function:
+	a. set the global variable to setInterval. The inputs of this method should be a function and the amount of milliseconds before the method runs again. The input-function will be called "countDown"
+
+10. the countDown fucntion:
+	a. will decrease the global-variable count by one
+	b. check/compare if the count is equal to 0
+	c. clear the setInterval (stop the setInterval function/method)
+	d. hide the div that contains all the questions
+	e. display the # of correct answers, incorrect ans, and unanswered by...
+
+11. Running a function "grade" that will:
+	a.interatively compare the users input.values to the solutions-array
+	b. update the global counts for correct,incorrect, unanswered qustions
---
+++ assets/javascript/game.js
@@ -0,0 +1,124 @@
+//------------------------------Making the Question/Answers for the Triva Game---------------//
+
+
+
+//making an array that will store the value of my pontential answers --> each question will have 4 answer with the respective values ...
+var abcd = ["a" , "b", "c", "d"];
+
+//making an object that will store my questions and the potential answers. I will use an iterative function later to loop through this object and to make the needed html code
+var allQs = {
+	Q1:"What's the core of Harry Potter's wand made of?" ,
+	ans1: ["Unicorn Hair" , "Troll Snot" , "Pixie Dust", "Pheonix Feather"],
+
+	Q2:"Who is Harry Potter's godfather?",
+	ans2: ["Remus Lupin", "Peter Pettigrew", "Sirius Black", "James Potter"],
+
+	Q3: "What animagus is Rita Skeeter?",
+	ans3:["Squirrel", "Dog", "Lady Bug", "ButteryFly"],
+
+	Q4: "In what year of Hogwarts did Hermoine first make PollyJuice potion?",
+	ans4: ["Year 1", "Year 2", "Year 3", "Year 4"],
+
+	Q5: "In Harry's 4th year at Hogwarts, who entered his name into the Goblet of Fire?",
+	ans5: ["Bartimus Crouch", "Mad-Eye Moody", "Bartimus Crouch Jr.", "Headmaster Dumbledore"],
+
+	Q6: "In his 5th year, which creature has Harry able to see upon his <em>arrival</em> to Hogwarts?",
+	ans6: ["Hippogriffs", "Thestrals", "Nargles", "Unicorns"],
+
+	Q7: "What was Professor Umbridge's first name?",
+	ans7: ["Susan", "Delfina", "Dolores", "Fleur"],
+
+	Q8: "Who is the self-proclaimed Half-Blood Prince?",
+	ans8: ["Harry Potter", "Tom Riddle", "Severus Snape", "Professor Slughorn"],
+
+	Q9: "What was Lord Voldemort researching (then making) during his youth to achieve ''immortality''? ",
+	ans9: ["Unicorn Blood", "Deathly Hallows", "PolyJuice Potion", "Horcruxes"],
+
+	Q10: "Who kills Nagini in the 'Battle of Hogwarts'?",
+	ans10: ["Neville Longbottom", "Ron Weasley", "Caretaker Finch", "Seamus Finnagan"]
+};
+//an array that stores the solutions to my questions in the form of a, b, c, or d --> corresponds the order the answers are in and the values I will assign them
+var sol = ["d","c","c","b","c","b", "c","c","d","a"];
+
+
+	//this function will go through my object (which alternates content in the format question, answers, question, answers,)
+	function makeQ(){
+		//my first loop will go through and select the question to write to the DOM. This needs to happen for HALF the length of my object --> half are questions and half are answers. Because each element is named in a similar fashion, I can index the by name and my iterator 'i'
+		for (i = 1; i < (Object.keys(allQs).length+ 1)/2; i++){
+
+			// I will define a local variable that store the current html for the DOM. 
+			var htmlQ = undefined;
+			htmlQ = "<li id = "+ 'ques' + i+ " > <strong>" + allQs["Q"+i] + "</strong><br><ul>";
+
+				//once a question is picked, I need to loop throught the array of answers and write them as part of the html.
+				for (j = 1; j <5; j++){
+					//name = q1 q2 q3...    value = a b c or d     id == q1a,q1b, ...q10a, q10c, q10d
+					htmlQ += " <input  type = 'radio' name = " + "q" +i + " value = '" + abcd[j-1] + "'" + "id = '" + "q" + i + abcd[j-1] + "'>" + allQs["ans"+i][j-1] + "          ";
+				}
+
+			//ending my html unorder-list of answers and adding 2 breaks
+			htmlQ += "</ul><br><br>";
+
+			//writing all html to the div .orderedQs (I will hide this div when the timer is up)
+
+			$(".orderedQs").append(htmlQ);
+		}
+		
+	};
+
+
+
+
+// ---------------------Making the Timer for the Trivia Game --------------//
+// where to start???
+
+//Making global variables that will store the # of correct answers, wrong answers, and unanswered questions
+var cAns = 0;
+var wAns = 0;
+var uAns = 0;
+
+var count = 60; // will keep track of my count, starting with 80
+var down1; //will store the setInterval (I need to figure this one out)
+$(".start").on("click",startSetup);
+
+function startSetup(){
+	$(".start").hide();
+	$(".time").html("<h1> Time remaining: " +count + " seconds </h1>");
+	makeQ();
+	//call my function so that it executes and writes my questions/answers to the page
+	keepGoing();
+}
+
+function countDown(){
+	count--;
+	$(".time").html("<h1> Time remaining: " +count + " seconds </h1>"); 
+	if (count ==0){
+		grade();
+		var theDisp = "<h1>Time is Up!</h1> <br><br> Correct Answers: " + cAns + "<br> Wrong Answers: " + wAns  + "<br>Unanswered: " + uAns;
+		$(".time").html(theDisp);
+		clearInterval(down1);
+		$(".theQs").hide();
+		count = 80;
+	}
+}
+
+function grade(){
+	//going to "GRADE" the users selected answers and modify the counts to be displayed. Going to loop thru all 10 questions to compare the values click to the solutions I have made/stored in the var "sol"
+
+	for(k = 1; k<11; k++){
+		if ($("input[name = 'q" + k+ "']:checked ").val() ==sol[k-1]){
+			cAns++;
+		}
+		else if ($("input[name = 'q" + k + "']:checked").val() == undefined){
+			uAns++;
+
+		}
+		else{
+			wAns++;
+		}
+	}
+}
+
+function keepGoing(){
+	down1 = setInterval(countDown,1000);
+}
